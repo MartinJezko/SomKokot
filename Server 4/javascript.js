a2e26d17214ed0d8c176d1968465cbf12bf4d8ec
Martin Ježko <47401139+MartinJezko@users.noreply.github.com>
2022-04-10 14:47:01 +0200

    Add files via upload
    + Fyzika - kmitavy pohyb

---
+++ Server 4/javascript.js
@@ -0,0 +1,9 @@
+function colorLight() {
+    document.body.style.backgroundColor = "#f0f8ff"
+    document.body.style.color = "black"
+}
+
+function colorDark() {
+    document.body.style.backgroundColor = "#191919"
+    document.body.style.color = "#FFFFFF"
+}
